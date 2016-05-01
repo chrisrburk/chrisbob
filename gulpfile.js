7a985e0aafee253bbe34e5a42984289bbe4eda2b
Chris Burk <cburk@pier1.com>
2016-05-01 18:11:27 -0500

    added browser sync module and gulp tasks to support

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,33 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
 
+//Hello function for testing gulp
 gulp.task('hello', function() {
   console.log('I\'m not your monkey :|');
 });
 
+//Sass complie function
 gulp.task('sass', function(){
   return gulp.src('app/scss/**/*.scss')
     .pipe(sass()) // Converts Sass to CSS with gulp-sass
     .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
-gulp.task('watch', function(){
+gulp.task('load-browser', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
+})
+
+gulp.task('watch', ['load-browser','sass'], function(){
   gulp.watch('app/scss/**/*.scss', ['sass']);
-  // Other watchers
+  // Reloads the browser whenever HTML or JS files change
+  gulp.watch('app/*.html', browserSync.reload);
+  gulp.watch('app/js/**/*.js', browserSync.reload);
 })
