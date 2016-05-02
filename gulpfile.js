c0595ec1decbfae91beddd75f764ecdc1f177585
Chris Burk <cburk@pier1.com>
2016-05-02 09:11:24 -0500

    adding gulp-cache, gulp-css-nano. gulp-imagemin, gulp-useref, gulp-autoprefixer, run-sequence & del

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,12 @@ var useref = require('gulp-useref');
 var uglify = require('gulp-uglify');
 var gulpIf = require('gulp-if');
 var cssnano = require('gulp-cssnano');
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
+var del = require('del');
+var runSequence = require('run-sequence');
+var autoprefixer = require('gulp-autoprefixer');
 
-//Hello function for testing gulp
-gulp.task('hello', function() {
-  console.log('I\'m not your monkey :|');
-});
 
 //Sass complie function
 gulp.task('sass', function(){
@@ -25,19 +26,12 @@ gulp.task('sass', function(){
 gulp.task('load-browser', function() {
   browserSync.init({
     server: {
-      baseDir: 'dist'
+      baseDir: 'app'
     },
   })
 })
 
-//Concat all build files into on master file
-gulp.task('useref', function(){
-  return gulp.src('app/*.html')
-    .pipe(useref())
-    .pipe(gulp.dest('dist'))
-});
-
-//Minify Js
+//Concat/minify all files into on master file in dist
 gulp.task('useref', function(){
   return gulp.src('app/*.html')
     .pipe(useref())
@@ -48,9 +42,49 @@ gulp.task('useref', function(){
     .pipe(gulp.dest('dist'))
 });
 
+//Optimize images
+gulp.task('images', function(){
+  return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
+  // Caching images that ran through imagemin
+  .pipe(cache(imagemin({
+      interlaced: true
+    })))
+  .pipe(gulp.dest('dist/images'))
+});
+
+//Clear the cache
+gulp.task('cache:clear', function (callback) {
+return cache.clearAll(callback)
+})
+
+//Move fonts from dev to dist
+gulp.task('fonts', function() {
+  return gulp.src('app/fonts/**/*')
+  .pipe(gulp.dest('dist/fonts'))
+})
+
+//Clean dist folder
+gulp.task('clean:dist', function() {
+  return del.sync('dist');
+})
+
+//Main watch function which complies sass and launches a browser
 gulp.task('watch', ['load-browser','sass'], function(){
   gulp.watch('app/scss/**/*.scss', ['sass']);
   // Reloads the browser whenever HTML or JS files change
   gulp.watch('app/*.html', browserSync.reload);
   gulp.watch('app/js/**/*.js', browserSync.reload);
+})
+
+gulp.task('default', function (callback) {
+  runSequence(['sass','load-browser', 'watch'],
+    callback
+  )
+})
+
+gulp.task('build', function (callback) {
+  runSequence('clean:dist',
+    ['sass', 'useref', 'images', 'fonts'],
+    callback
+  )
 })
