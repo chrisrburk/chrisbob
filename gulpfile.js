e3e540f064315114b42b323220a01432791b842c
Chris Burk <cburk@pier1.com>
2016-05-20 00:36:49 -0500

    updated sass output style to expanded

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,7 @@ var autoprefixer = require('gulp-autoprefixer');
 //Sass complie function
 gulp.task('sass', function(){
   return gulp.src('app/scss/**/*.scss')
-    .pipe(sass()) // Converts Sass to CSS with gulp-sass
+    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError)) // Converts Sass to CSS with gulp-sass
     .pipe(autoprefixer())
     .pipe(gulp.dest('app/css'))
     .pipe(browserSync.reload({
